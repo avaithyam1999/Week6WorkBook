f423672d7e5c95bb0891a8860e9fe198a811e398
AJ V <avaithyam@yahoo.com>
2025-10-29 14:15:44 -0400

    dog.

--- src/main/java/com/pluralsight/assetmgr/Program.java
+++ src/main/java/com/pluralsight/assetmgr/Program.java
@@ -8,6 +8,8 @@ public class Program {
         Asset house1 = new House("Ajith's House", "10-20-2025", 500000.99, "2020 Melbourne DR", 5, 45000, 23);
         Asset car1 = new Vehicle("Ajith's whip", "5-5-2025", 69450.95, "Honda Civic Type R", 2025, 8450);
 
+        assets.add(house1);
+        assets.add(car1);
         System.out.println(house1.getValue());
         System.out.println((car1.getValue()));
     }
